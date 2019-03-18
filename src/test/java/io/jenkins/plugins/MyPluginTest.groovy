ea41c9b9d178ca18989c6e7bbc5a5599abfdc4a0
Brady Wied <brady@bswtechconsulting.com>
2019-03-18 09:55:01 -0600

    Got a test to run

---
+++ src/test/java/io/jenkins/plugins/MyPluginTest.groovy
@@ -0,0 +1,31 @@
+package io.jenkins.plugins
+
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition
+import org.jenkinsci.plugins.workflow.job.WorkflowJob
+import org.junit.ClassRule
+import org.junit.Rule
+import org.junit.Test
+import org.jvnet.hudson.test.BuildWatcher
+import org.jvnet.hudson.test.JenkinsRule
+
+class MyPluginTest {
+    @Rule
+    public JenkinsRule j = new JenkinsRule()
+    @ClassRule
+    public static BuildWatcher bw = new BuildWatcher()
+
+    @Test
+    void foo() {
+        // arrange
+        def project = j.createProject(WorkflowJob)
+        project.definition = new CpsFlowDefinition("node { echo 'hello' }")
+
+        // act
+        def build = j.buildAndAssertSuccess(project)
+
+        // assert
+        j.assertLogContains('howdy',
+                            build)
+        fail 'write the test'
+    }
+}
