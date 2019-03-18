c2ba2bc0e466e4f3e270c9fd90facf7c5a4eb7d2
Brady Wied <brady@bswtechconsulting.com>
2019-03-18 11:15:56 -0600

    Got our method to at least run

--- src/main/java/io/jenkins/plugins/MyPlugin.java
+++ src/main/java/io/jenkins/plugins/MyPlugin.java
@@ -1,17 +1,14 @@
 package io.jenkins.plugins;
 
-import hudson.FilePath;
-import hudson.Launcher;
-import hudson.model.Run;
-import hudson.model.TaskListener;
-import hudson.tasks.Builder;
-import jenkins.tasks.SimpleBuildStep;
+import hudson.Extension;
+import org.jenkinsci.plugins.workflow.steps.*;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 import javax.annotation.Nonnull;
-import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
-public class MyPlugin extends Builder implements SimpleBuildStep {
+public final class MyPlugin extends Step {
     private final String theParameter;
 
     @DataBoundConstructor
@@ -20,7 +17,37 @@ public class MyPlugin extends Builder implements SimpleBuildStep {
     }
 
     @Override
-    public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath workspace, @Nonnull Launcher launcher, @Nonnull TaskListener listener) throws InterruptedException, IOException {
-        listener.getLogger().println("we ran with parameter " + this.theParameter);
+    public StepExecution start(StepContext context) throws Exception {
+        return new Execution(context, this);
+    }
+
+    public static final class Execution extends SynchronousNonBlockingStepExecution {
+        // TODO: Is this the right way to do this? It's serializable (the step isn't so we can't put that here)
+        private String theParameter;
+
+        protected Execution(@Nonnull StepContext context,
+                            MyPlugin step) {
+            super(context);
+            this.theParameter = step.theParameter;
+        }
+
+        @Override
+        protected Void run() throws Exception {
+            System.out.println("hi "+ theParameter);
+            return null;
+        }
+    }
+
+    @Extension
+    public static class DescriptorImpl extends StepDescriptor {
+        @Override
+        public Set<? extends Class<?>> getRequiredContext() {
+            return new HashSet<>();
+        }
+
+        @Override
+        public String getFunctionName() {
+            return "doStuff";
+        }
     }
 }
